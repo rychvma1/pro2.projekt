3aedd0f8cbb931bab301b57eaf5b22880db5b6fe
rychvma1 <rychvma1@J09-15.uhk.cz>
2016-10-19 16:03:40 +0200

    prvy commit

---
+++ src/app/Svet.java
@@ -0,0 +1,10 @@
+package app;
+
+public class Svet {
+
+	public static void main(String[] args) {
+		System.out.println("Bu");
+
+	}
+
+}
---
+++ src/model/Srdce.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Srdce {
+
+	private int velikost;
+	private int pozX, pozY;
+	
+}
